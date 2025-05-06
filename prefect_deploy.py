a822ba278766470addae574c6d7a3633832ef06b
ssmulyan <40801004+ssmulyan@users.noreply.github.com>
2025-05-05 20:30:09 -0400

    adding prefect deploy config

--- prefect_deploy.py
+++ prefect_deploy.py
@@ -1,12 +1,8 @@
-from prefect.deployments import Deployment
-from prefect.server.schemas.schedules import CronSchedule
 from app import main
 
-# Define and register the deployment
-Deployment.build_from_flow(
-    flow=main,
-    name="Daily ETL",
-    schedule=(CronSchedule(cron="0 12 * * *", timezone="America/New_York")),  # Runs daily at noon ET
-    work_pool_name="default",
-    tags=["etl", "daily"]
-).apply()
+if __name__ == "__main__":
+    main.deploy(
+        name="Daily ETL",
+        work_pool_name="default",
+        tags=["etl", "daily"]
+    )
