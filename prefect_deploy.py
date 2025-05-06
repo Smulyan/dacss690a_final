5b848cc44425212c0785b6a1800dd98cd89d373d
ssmulyan <40801004+ssmulyan@users.noreply.github.com>
2025-05-06 12:41:51 -0400

    updating prefect deploy config

--- prefect_deploy.py
+++ prefect_deploy.py
@@ -1,9 +1,9 @@
 from prefect import serve
-from prefect.server.schemas.schedules import CronSchedule
+from prefect.client.schemas.schedules import CronSchedule
 from app import main_flow
 
 if __name__ == "__main__":
     main_flow.serve(
         name="Daily ETL",
-        Schedule=CronSchedule(cron="0 12 * * *", timezone="America/New_York")
+        schedules=[CronSchedule(cron="0 12 * * *", timezone="America/New_York")]
     )
