e3e1dad6afb02d3dbebae9b43176f04c5659de06
ssmulyan <40801004+ssmulyan@users.noreply.github.com>
2025-05-09 13:08:39 -0400

    changing streamlit app title

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ def load_data():
 df = load_data()
 
 st.title("Subject and Language Trends in Crossref Abstracts")
+st.title("Daily classification using spaCy NLP")
 
 # Sidebar filters
 st.sidebar.header("Filter options")
